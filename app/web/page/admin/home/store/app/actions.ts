be18353822df7ab0b22330f23a8cc926a73fa1de
caoli <cl98914@alibaba-inc.com>
2018-03-16 20:33:31 +0800

    fix: axios cookie missing

--- app/web/page/admin/home/store/app/actions.ts
+++ app/web/page/admin/home/store/app/actions.ts
@@ -12,22 +12,29 @@ const host = 'http://localhost:7001';
 const actions = {
 
   FETCH_ARTICLE_LIST: ({ commit, dispatch, state }, query) => {
-    return axios.post(`${host}/admin/api/article/list`, { ...{ _csrf: state.csrf }, ...query })
-        .then(response => {
-          commit(Type.SET_ARTICLE_LIST, response.data);
-        });
+    return axios.post(`${host}/admin/api/article/list`, query, {
+      headers: {
+        'x-csrf-token': state.csrf,
+        'Cookie': `csrfToken=${state.csrf}`
+      }
+    }).then(response => {
+      commit(Type.SET_ARTICLE_LIST, response.data);
+    });
   },
 
   FETCH_ARTICLE_DETAIL: ({ commit, dispatch, state }, { id }) => {
     return axios.get(`${host}/admin/api/article/${id}`)
-    .then(response => {
-      commit(Type.SET_ARTICLE_DETAIL, response.data);
-    });
+      .then(response => {
+        commit(Type.SET_ARTICLE_DETAIL, response.data);
+      });
   },
 
   SAVE_ARTICLE: ({ commit, dispatch, state }, data) => {
-    return axios.post(`${host}/admin/api/article/add`, { ...{ _csrf: state.csrf }, ...data })
-    .then(response => {
+    return axios.post(`${host}/admin/api/article/add`, data, {
+      headers: {
+        'x-csrf-token': state.csrf,
+      }
+    }).then(response => {
       commit(Type.SET_ARTICLE_LIST, data);
     });
   },
